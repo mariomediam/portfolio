f468e8dc9dfae7cfbc844b30efea7f5ccb3265cd
Your Name <you@example.com>
2023-12-01 13:42:18 -0500

    Publish netlify

--- superior-shepherd/astro.config.mjs
+++ superior-shepherd/astro.config.mjs
@@ -1,18 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import node from "@astrojs/node";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   // output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: netlify()
   // vite: {
   //   ssr: {
   //     noExternal: ["/app/node_modules"],
   //   },
   // },
+  ,
+  output: "server"
 });
