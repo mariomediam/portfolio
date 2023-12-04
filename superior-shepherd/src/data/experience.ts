8ba905c439a41bdda9015092fb3e1222d5c83341
Your Name <you@example.com>
2023-12-04 09:24:48 -0500

    Publish netlify

--- superior-shepherd/src/data/experience.ts
+++ superior-shepherd/src/data/experience.ts
@@ -4,28 +4,28 @@ const experience = [
         "measure": "5M",
         "subtitle": "Documentos registrados",
         "description": "A la fecha, el sistema ha registrado exitosamente más de 5 millones de documentos, lo que ha generado notables beneficios en términos de eficiencia, organización y accesibilidad de la información.",
-        "icon": "major"
+        "icon": "Major"
       },
       {
         "title": "Sistema de licencias de conducir",
         "measure": "47K",
         "subtitle": "Licencias emitidas",
         "description": "Hemos logrado agilizar significativamente el proceso de obtención de licencias de conducir de vehículos menores, reduciendo los tiempos de espera y mejorando la experiencia de los usuarios.",
-        "icon": "major"
+        "icon": "Major"
       },
       {
         "title": "Sistema de Fiscalización",
         "measure": "112K",
         "subtitle": "Sanciones impuestas",
         "description": "Permite realizar un seguimiento detallado de las infracciones de tráfico y otros incidentes, facilitando la identificación de patrones, la asignación de recursos y la implementación de medidas preventivas.",
-        "icon": "major"
+        "icon": "Major"
       },
       {
         "title": "Sistema de licencias de funcionamiento",
         "measure": "18K",
         "subtitle": "Autorizaciones emitidas",
         "description": "Su implementación ha logrado simplificar procesos, facilitado la apertura rápida de negocios, estimulando la actividad económica y generando empleo.",
-        "icon": "major"
+        "icon": "Major"
       },
       {
         "title": "Administrador de Base de Datos",
