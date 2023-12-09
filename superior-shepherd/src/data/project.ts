c141d67406b19c5eaaba6740f9e0095515a44474
Your Name <you@example.com>
2023-12-08 23:20:54 -0500

    Section Projects

---
+++ superior-shepherd/src/data/project.ts
@@ -0,0 +1,198 @@
+const project = [
+  {
+    title: "Licencias provisionales",
+    description:
+      "Permite a los emprendedores obtener rápidamente las autorizaciones necesarias para desplegar sus ideas y proyectos en los espacios públicos de la ciudad de Piura - Perú.",
+    bg_color: "df-title",
+    text_color: "df-background",
+    images: [
+      "/assets/screens/provisional1.webp",
+      "/assets/screens/provisional2.webp",
+      "/assets/screens/provisional3.webp",
+      "/assets/screens/provisional4.webp",
+    ],
+    technologies: [
+      {
+        image: "react",
+        name: "React",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "redux",
+        name: "Redux",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "bootstrap",
+        name: "Bootstrap",
+        size: { width: "42px", height: "42px" },
+      },
+      {
+        image: "django",
+        name: "Django",
+        size: { width: "35px", height: "35px" },
+      },
+      {
+        image: "sql-server",
+        name: "MSSQL",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "apache",
+        name: "Apache",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "ubuntu",
+        name: "Ubuntu",
+        size: { width: "40px", height: "40px" },
+      },
+    ],
+  },
+  {
+    title: "Contribuyentes",
+    description:
+      "Sistema para la gestión de contribuyentes, integrado con el Registro Nacional de Identificación y Estado Civil (RENIEC) y la Superintendencia Nacional de Aduanas y de Administración Tributaria (SUNAT) minimizando de manera significativa los riesgos de datos incorrectos, fortaleciendo así la eficacia de la administración tributaria",
+    bg_color: "df-bg-section",
+    text_color: "df-text",
+    images: [
+      "/assets/screens/contribuyentes1.webp",
+      "/assets/screens/contribuyentes2.webp",
+      "/assets/screens/contribuyentes3.webp",
+      "/assets/screens/contribuyentes4.webp",
+    ],
+    technologies: [
+      {
+        image: "react",
+        name: "React",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "redux",
+        name: "Redux",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "bootstrap",
+        name: "Bootstrap",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "django",
+        name: "Django",
+        paddingAndMargin: "py-0.5 m-0",
+      },
+      {
+        image: "sql-server",
+        name: "MSSQL",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "apache",
+        name: "Apache",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "ubuntu",
+        name: "Ubuntu",
+        size: { width: "40px", height: "40px" },
+      },
+    ],
+  },
+  {
+    title: "Recibos de salario",
+    description:
+      "Permite la generación y envío de recibos de salario en formato PDF firmado digitalmente, permitiendo un ahorro sustancial de recursos como papel y personal, al mismo tiempo que maximiza la eficiencia al eliminar la necesidad de desplazamientos físicos para la obtención de recibos.",
+    bg_color: "df-yellow-dark",
+    text_color: "df-background",
+    images: [
+      "/assets/screens/rrhh1.webp",
+      "/assets/screens/rrhh2.webp",
+      "/assets/screens/rrhh3.webp",
+    ],
+    technologies: [
+      {
+        image: "react",
+        name: "React",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "redux",
+        name: "Redux",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "bootstrap",
+        name: "Bootstrap",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "django",
+        name: "Django",
+        paddingAndMargin: "py-0.5 m-0",
+      },
+      {
+        image: "sql-server",
+        name: "MSSQL",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "apache",
+        name: "Apache",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "ubuntu",
+        name: "Ubuntu",
+        size: { width: "40px", height: "40px" },
+      },
+    ],
+  },
+  {
+    title: "Chatbot inteligente",
+    description:
+      "Entrenado con los procedimientos administrativos y servicios exclusivos ofrecidos por la Municipalidad, permite a los ciudadanos realizar consultas en lenguaje natural, simplificando drásticamente la comprensión y navegación a través de la complejidad de la información administrativa.",
+    bg_color: "df-title",
+    text_color: "df-background",
+    images: ["/assets/screens/chat1.webp"],
+    technologies: [
+      {
+        image: "react",
+        name: "React",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "redux",
+        name: "Redux",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "bootstrap",
+        name: "Bootstrap",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "django",
+        name: "Django",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "sql-server",
+        name: "MSSQL",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "apache",
+        name: "Apache",
+        size: { width: "40px", height: "40px" },
+      },
+      {
+        image: "ubuntu",
+        name: "Ubuntu",
+        size: { width: "40px", height: "40px" },
+      },
+    ],
+  },
+];
+
+export default project;
